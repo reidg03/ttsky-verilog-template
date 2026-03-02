16b30790b0e020d30ddff9607d61800d97db29c9
reidg03 <130784736+reidg03@users.noreply.github.com>
2026-03-02 15:18:47 -0800

    uploaded verilog files

---
+++ src/Main.v
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2026 Reid Graham
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module Main (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+wire [3:0] A;
+wire [3:0] B;
+wire [7:0] P;
+
+// split the inputs
+assign A = ui_in[3:0];
+assign B = ui_in[7:4];
+
+// instantiate your multiplier
+CarryPropArrMul mul (
+    .A(A),
+    .B(B),
+    .P(P)
+);
+
+// connect to outputs
+assign uo_out = P;
+
+// unused bidirectional IO
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
+
+// prevent unused warnings
+wire _unused = &{uio_in, ena, clk, rst_n, 1'b0};
+
+endmodule
---
+++ src/MulAdder.v
@@ -0,0 +1,19 @@
+module MulAdder(
+    input X,
+    input Y,
+    input Sin,
+    input Cin,
+    output Sout,
+    output Cout,
+    output Xout,
+);
+
+FullAdder fa (
+    .A(Sin),
+    .B(X & Y),
+    .Cin(Cin),
+    .S(Sout),
+    .Cout(Cout)
+);
+
+endmodule
