26697a86667bdde15041429a43513c173c1a7dad
reidg03 <130784736+reidg03@users.noreply.github.com>
2026-03-03 12:49:09 -0800

    restored tb.v to original

--- test/tb.v
+++ test/tb.v
@@ -27,46 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-
-   //TESTING
-  initial begin
-    // Start with reset low
-    rst_n = 0;
-    ena   = 1;
-    ui_in = 8'b00000000;
-    uio_in = 8'b00000000;
-
-    #5 rst_n = 1;  // release reset
-
-    // Test case 1: 3 * 2
-    ui_in = {4'd3, 4'd2};
-    #5;
-    $display("A=3 B=2 -> P=%d", uo_out);
-     assert(uo_out == 8'd6) else $fatal(1, "3*2 failed");
-
-    // Test case 2: 4 * 5
-    ui_in = {4'd4, 4'd5};
-    #5;
-    $display("A=4 B=5 -> P=%d", uo_out);
-     assert(uo_out == 8'd20) else $fatal(1, "4*5 failed");
-
-   // Test case 3: 12 * 15
-        ui_in = {4'd12, 4'd15};
-    #5;
-     $display("A=12 B=15 -> P=%d", uo_out);
-     assert($unsigned(uo_out) == 8'd180) else $fatal(1, "12*15 failed");
-
-   // Test case 4: 13 * 7
-        ui_in = {4'd13, 4'd7};
-    #5;
-     $display("A=13 B=7 -> P=%d", uo_out);
-     assert(uo_out == 8'd91) else $fatal(1, "13*7 failed");
-
-    $finish;
-  end
-   
   // Replace tt_um_example with your module name:
-  tt_um_reidg03_gmult user_project (
+  tt_um_example user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -85,3 +47,91 @@ module tb ();
   );
 
 endmodule
+
+// `default_nettype none
+// `timescale 1ns / 1ps
+
+// /* This testbench just instantiates the module and makes some convenient wires
+//    that can be driven / tested by the cocotb test.py.
+// */
+// module tb ();
+
+//   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
+//   initial begin
+//     $dumpfile("tb.fst");
+//     $dumpvars(0, tb);
+//     #1;
+//   end
+
+//   // Wire up the inputs and outputs:
+//   reg clk;
+//   reg rst_n;
+//   reg ena;
+//   reg [7:0] ui_in;
+//   reg [7:0] uio_in;
+//   wire [7:0] uo_out;
+//   wire [7:0] uio_out;
+//   wire [7:0] uio_oe;
+// `ifdef GL_TEST
+//   wire VPWR = 1'b1;
+//   wire VGND = 1'b0;
+// `endif
+
+
+//    //TESTING
+//   initial begin
+//     // Start with reset low
+//     rst_n = 0;
+//     ena   = 1;
+//     ui_in = 8'b00000000;
+//     uio_in = 8'b00000000;
+
+//     #5 rst_n = 1;  // release reset
+
+//     // Test case 1: 3 * 2
+//     ui_in = {4'd3, 4'd2};
+//     #5;
+//     $display("A=3 B=2 -> P=%d", uo_out);
+//      assert(uo_out == 8'd6) else $fatal(1, "3*2 failed");
+
+//     // Test case 2: 4 * 5
+//     ui_in = {4'd4, 4'd5};
+//     #5;
+//     $display("A=4 B=5 -> P=%d", uo_out);
+//      assert(uo_out == 8'd20) else $fatal(1, "4*5 failed");
+
+//    // Test case 3: 12 * 15
+//         ui_in = {4'd12, 4'd15};
+//     #5;
+//      $display("A=12 B=15 -> P=%d", uo_out);
+//      assert($unsigned(uo_out) == 8'd180) else $fatal(1, "12*15 failed");
+
+//    // Test case 4: 13 * 7
+//         ui_in = {4'd13, 4'd7};
+//     #5;
+//      $display("A=13 B=7 -> P=%d", uo_out);
+//      assert(uo_out == 8'd91) else $fatal(1, "13*7 failed");
+
+//     $finish;
+//   end
+   
+//   // Replace tt_um_example with your module name:
+//   tt_um_reidg03_gmult user_project (
+
+//       // Include power ports for the Gate Level test:
+// `ifdef GL_TEST
+//       .VPWR(VPWR),
+//       .VGND(VGND),
+// `endif
+
+//       .ui_in  (ui_in),    // Dedicated inputs
+//       .uo_out (uo_out),   // Dedicated outputs
+//       .uio_in (uio_in),   // IOs: Input path
+//       .uio_out(uio_out),  // IOs: Output path
+//       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+//       .ena    (ena),      // enable - goes high when design is selected
+//       .clk    (clk),      // clock
+//       .rst_n  (rst_n)     // not reset
+//   );
+
+// endmodule
